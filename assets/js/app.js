51b93e1fd078af2e1f01a370a32ca61f2238b222
nguyenvanbao <bao19042004@gmail.com>
2022-01-25 21:30:23 +0700

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
 import songs from "./data.js";
 const playList = document.querySelector('.song-list')
 const audio = document.querySelector('.audio')
+const cdThumb = document.querySelector('.music-player-img')
+    const cdThumbAnimate = cdThumb.animate([
+        {transform: 'rotate(360deg)'}
+    ],{
+        duration: 10000,
+        iterations: Infinity
+    })
+
 var isPlaying = false
 
 
@@ -8,9 +16,27 @@ function start(){
     renderSongs()
     clickSong()
     playPauseBtn()
+    showMusicPlayer()
+    handelMusicPlayer()
 }
 start()
 
+
+function formatTime(sec_num) {
+    let hours = Math.floor(sec_num / 3600);
+    let minutes = Math.floor((sec_num - hours * 3600) / 60);
+    let seconds = Math.floor(sec_num - hours * 3600 - minutes * 60);
+
+    hours = hours < 10 ? (hours > 0 ? '0' + hours : 0) : hours;
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+    return (hours !== 0 ? hours + ':' : '') + minutes + ':' + seconds;
+}
 
 function renderSongs(){
     const htmls = songs.map(function(song,index){
@@ -33,14 +59,25 @@ function clickSong(){
     const footer = document.querySelector('.footer')
     const songItem = document.querySelectorAll('.song-item')
     const name = document.querySelector('.name')
-    
+    const singer = document.querySelector('.singer')
     const player = document.querySelector('.player')
 
+    const playingImg = document.querySelector('.music-player-img')
+    const playingName = document.querySelector('.music-player-name')
+    const playingSinger = document.querySelector('.music-player-singer')
+    
     songItem.forEach(function(song,index){
         song.onclick = function(){
             player.classList.add('playing')
             name.textContent = songs[index].name
+            singer.textContent = songs[index].singer
             audio.src= songs[index].src
+
+            playingImg.src=songs[index].img
+            playingName.textContent = songs[index].name
+            playingSinger.textContent = songs[index].singer
+            
+            //run
             footer.classList.remove("hiden")
             isPlaying = true
             audio.play()
@@ -57,11 +94,88 @@ function playPauseBtn(){
                 isPlaying = false
                 player.classList.remove('playing')
                 audio.pause()
+                cdThumbAnimate.pause()
             } else{
                 isPlaying = true
                 player.classList.add('playing')
                 audio.play()
+                cdThumbAnimate.play()
             }
         }
     })
+}
+
+function showMusicPlayer(){
+        const footer = document.querySelector('.footer')
+        const spanIconPause = document.querySelector('.player span:first-child')
+        const spanIconPlay = document.querySelector('.player span:last-of-type')
+        const iconPause = document.querySelector('.icon-pause')
+        const iconPlay = document.querySelector('.icon-play')
+        const musicPlayer = document.querySelector('.music-player-container')
+    
+        footer.onclick = function(e){
+            if(e.target != spanIconPause && e.target != spanIconPlay &&
+                 e.target != iconPause && e.target != iconPlay){
+                    musicPlayer.classList.add('show')
+                    checkPlaying()
+            }
+        }
+}
+
+function handelMusicPlayer(){
+    const playingTimeEnd = document.querySelector('.playing-time-end')
+    const playingTimeStart = document.querySelector('.playing-time-start')
+    const range = document.querySelector('.range')
+    const player = document.querySelector('.play-or-pause')
+    const btns = document.querySelectorAll('.play-or-pause span')
+    const backBtn = document.querySelector('.back-btn')
+    const musicPlayer = document.querySelector('.music-player-container')
+    const iconHeart = document.querySelector('.icon-heart')
+
+    function setTimeStart(){
+        playingTimeStart.textContent = formatTime(audio.currentTime)
+    }
+    setInterval(setTimeStart)
+    function getTimeEnd(){
+        playingTimeEnd.textContent = formatTime(audio.duration)
+    }
+    setTimeout(getTimeEnd,3000)
+
+    range.onchange = function(e){
+        const seekTime = audio.duration/100*e.target.value
+        audio.currentTime = seekTime
+    }
+    audio.ontimeupdate = function(){
+        range.value = audio.currentTime / audio.duration *100
+    }
+    btns.forEach(function(btn){
+        btn.onclick = function(){
+            if(isPlaying){
+                isPlaying = false
+                player.classList.remove('playing')
+                audio.pause()
+                cdThumbAnimate.pause()
+            } else{
+                isPlaying = true
+                player.classList.add('playing')
+                audio.play()
+                cdThumbAnimate.play()
+            }
+        }
+    })
+    backBtn.onclick = function(){
+        musicPlayer.classList.remove('show')
+    }
+    iconHeart.onclick = function(){
+        iconHeart.classList.toggle('active')
+    }
+}
+
+function checkPlaying(){
+    const player = document.querySelector('.play-or-pause')
+    if(isPlaying){
+        player.classList.add('playing')
+    } else{
+        player.classList.remove('playing')
+    }
 }
--- assets/js/data.js
+++ assets/js/data.js
@@ -3,49 +3,57 @@ var songs = [
         name: "Mang tiền về cho mẹ",
         singer: "Đen Vâu",
         src: "assets/music/Mang Tien Ve Cho Me - Den Nguyen Thao (NhacPro.net).mp3",
-        time: "6:42"
+        time: "6:42",
+        img: "https://i.scdn.co/image/ab67616d00001e0249d5fbb90bd190dc81286c17"
     },
     {
         name: "Trốn tìm",
         singer: "Đen Vâu",
         src: "assets/music/Tron Tim - Den MTV (NhacPro.net).mp3",
-        time: "4:13"
+        time: "4:13",
+        img: "https://i.scdn.co/image/ab67616d00001e0243ad699e3f348cb8e8052246"
     },
     {
         name: "Một triệu like",
         singer: "Đen Vâu",
         src: "assets/music/[MP3DOWNLOAD.TO] Đen - một triệu like ft. Thành Đồng (M_V)-192k.mp3",
-        time: "4:22"
+        time: "4:22",
+        img: "https://i.scdn.co/image/ab67616d00001e02a0b8e11b0b27827b3a3dd10d"
     },
     {
         name: "Trời hôm nay nhiều mây cực",
         singer: "Đen Vâu",
         src: "assets/music/TroiHomNayNhieuMayCuc-Den-6464309.mp3",
-        time: "4:10"
+        time: "4:10",
+        img: "https://i.scdn.co/image/ab67616d00001e02b5f8f41876b80943b1921f6b"
     },
     {
         name: "Lối nhỏ",
         singer: "Đen Vâu",
         src: "assets/music/LoiNho1-DenPhuongAnhDao-6129215.mp3",
-        time: "4:57"
+        time: "4:57",
+        img: "https://i.scdn.co/image/ab67616d00001e02539ffc79122ccceb066f5340"
     },
     {
         name: "Đố em biết anh đang nghĩ gì",
         singer: "Đen Vâu",
         src: "assets/music/Do Em Biet Anh Dang Nghi Gi.m4a",
-        time: "4:07"
+        time: "4:07",
+        img: "https://i.scdn.co/image/ab67616d00001e0257523af3d07128304e422991"
     },
     {
         name: "Hai triệu năm",
         singer: "Đen Vâu",
         src: "assets/music/HaiTrieuNam-DenBien-6007307.mp3",
-        time: "3:38"
+        time: "3:38",
+        img: "https://i.scdn.co/image/ab67616d00001e02b725279ebb685c6de6c01b46"
     },
     {
         name: "Anh đếch cần gì nhiều ngoài em",
         singer: "Đen Vâu",
         src: "assets/music/AnhDechCanGiNhieuNgoaiEm-DenVuThanhDong-5749937.mp3",
-        time: "3:39"
+        time: "3:39",
+        img: "https://i.scdn.co/image/ab67616d00001e02babcd515ad455b2c49c54c96"
     }
 ]
 export default songs
